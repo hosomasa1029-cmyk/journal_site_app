7d05c194e846170109aabfe50cb53d4d6ebba34f
hosomasa1029 <hosomasa1029@gmail.com>
2025-09-03 08:23:37 +0900

    課題1完了

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+        // ArticleSeeder（シーダークラス）の呼び出し
+        if (config('app.env') == 'local') {
+            $this->call(ArticleSeeder::class);
+        }      // \App\Models\User::factory(10)->create();
     }
 }
---
+++ resources/views/livewire/articles/show.blade.php
@@ -0,0 +1,13 @@
+<?php
+
+use function Livewire\Volt\{state};
+use App\Models\Article;
+// ルートモデルバインディング
+state(['memo' => fn(Article $article) => $article]);
+?>
+
+<div>
+    <a href="{{ route('articles.index') }}">戻る</a>
+    <h1>{{ $memo->title }}</h1>
+    <p>{!! nl2br(e($memo->body)) !!}</p>
+</div>
